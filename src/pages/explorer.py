b50c4aef8ff963dcad7b7ae27dac60d142aee840
jacobelemoine <jacobelemoine@gmail.com>
2025-12-05 16:16:17 -0500

    Added comments, created line graphs of different parameters with labels and axes

--- src/pages/explorer.py
+++ src/pages/explorer.py
@@ -1,33 +1,43 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
-
+import matplotlib.pyplot as plt
 
 def app():
+    #Set title
     st.title("Air Quality Explorer")
 
-    df = pd.read_csv("data/sample_air_quality.csv", parse_dates=["timestamp"])
+    #Read data
+    df = pd.read_csv("data/openaq_newtonville.csv", parse_dates=["datetimeLocal"])
 
-    city = st.selectbox("City", sorted(df["city"].unique()))
+    #Create boxes for city and parameter choice - need to update with cities' id's
+    #Also need to change pm1 to PM10 and pm25 to PM2.5 in data cleaning
+    city = st.selectbox("City", sorted(df["location_id"].unique()))
     parameter = st.selectbox("Parameter", sorted(df["parameter"].unique()))
 
-    sel = df[(df["city"] == city) & (df["parameter"] == parameter)].copy()
+    #Get sub data frame using city and requested parameter
+    sel = df[(df["location_id"] == city) & (df["parameter"] == parameter)].copy()
 
+    #Make sure data exists in subdataframe
     if sel.empty:
         st.warning("No data for selection.")
         return
 
-    chart = (
-        alt.Chart(sel)
-        .mark_line(point=True)
-        .encode(x="timestamp:T", y="value:Q", tooltip=["timestamp", "value"]) 
-        .properties(height=300)
-    )
+    #Create line chart of chosen parameter over time
+    subdf = sel[["datetimeLocal", "value"]]
+    graph, axes = plt.subplots()
+    axes.plot(subdf["datetimeLocal"], subdf["value"])
 
-    st.altair_chart(chart.interactive(), use_container_width=True)
+    axes.set_title(f"{parameter} levels over time in {city}")
+    axes.set_xlabel("Date")
+    axes.set_ylabel(parameter)
 
+    st.pyplot(graph)
+    
+    #need to add bar chart of worst pollution days here or in risk?
+
+    #Display data and map
     st.subheader("Data")
-    st.dataframe(sel.sort_values("timestamp", ascending=False))
+    st.dataframe(sel.sort_values("datetimeLocal", ascending=False))
 
     st.subheader("Map (placeholder)")
     coords = sel[["latitude", "longitude"]].dropna().rename(columns={"latitude": "lat", "longitude": "lon"})
