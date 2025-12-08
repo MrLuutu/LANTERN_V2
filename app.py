dad21e80164f06c0c191fcff8ab5773fc20c2737
jacobelemoine <jacobelemoine@gmail.com>
2025-12-08 16:55:51 -0500

    Added comments to app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from src.pages import home, explorer, risk
 
+#Set page configuration
 st.set_page_config(page_title="Lantern Air Lite", layout="wide")
 
+#Set dict of page names to module names
 PAGES = {
     "Home": home,
     "Air Quality Explorer": explorer,
     "Asthma Risk": risk,
 }
 
+#Create sidebar of pages
 st.sidebar.title("Lantern Air Lite")
 selection = st.sidebar.radio("Navigate", list(PAGES.keys()))
 
+#Run the selected page
 page = PAGES[selection]
 page.app()
